98ac4ec5cbddc9c04d3bb0ff73a8574b46751d3f
Pablo <pvarber@outlook.es>
2019-12-18 15:16:24 -0600

    Installed some plugins

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,7 +18,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (minions org-bullets zenburn-theme use-package))))
+ '(package-selected-packages
+   (quote
+    (zenburn-theme which-key use-package org-bullets moody minions magit ivy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
