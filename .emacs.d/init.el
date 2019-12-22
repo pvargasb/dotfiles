aeebee83a2b325a37b190802b02363ac5d8bcad4
Pablo <pvarber@outlook.es>
2019-12-22 11:26:59 -0600

    Initial org setup and company settings changed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,7 +20,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (moody auto-compile company evil zenburn-theme which-key use-package org-bullets minions magit ivy evil-leader))))
+    (key-chord moody auto-compile company evil zenburn-theme which-key use-package org-bullets minions magit ivy evil-leader))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
