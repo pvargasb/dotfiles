fce239e471a6fc384fe024126e2c3bd900c14854
Pablo <pvarber@outlook.es>
2019-12-23 19:18:29 -0600

    Moved custom properties to different file

---
+++ .emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (vimish-fold evil-nerd-commenter helpful dumb-jump evil-surround key-chord moody auto-compile company evil zenburn-theme which-key use-package org-bullets minions magit ivy evil-leader))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,17 +13,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (vimish-fold evil-nerd-commenter helpful dumb-jump evil-surround key-chord moody auto-compile company evil zenburn-theme which-key use-package org-bullets minions magit ivy evil-leader))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
