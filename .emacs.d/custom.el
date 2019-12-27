9ab571fb7dd357e57b21103c30bf4deb64c67bbc
Pablo <pvarber@outlook.es>
2019-12-26 21:01:33 -0600

    Marked multiple cursors as installed

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (counsel which-key vimish-fold use-package org-bullets moody minions key-chord helpful gruvbox-theme evil-surround evil-nerd-commenter evil-magit evil-leader dumb-jump counsel-projectile company auto-compile))))
+    (multiple-cursors counsel which-key vimish-fold use-package org-bullets moody minions key-chord helpful gruvbox-theme evil-surround evil-nerd-commenter evil-magit evil-leader dumb-jump counsel-projectile company auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
