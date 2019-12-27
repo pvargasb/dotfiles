697decc90af1352e41976a8707d287b2ca834740
Pablo <pvarber@outlook.es>
2019-12-26 22:28:01 -0600

    Marked some packages as installed

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (multiple-cursors counsel which-key vimish-fold use-package org-bullets moody minions key-chord helpful gruvbox-theme evil-surround evil-nerd-commenter evil-magit evil-leader dumb-jump counsel-projectile company))))
+    (evil ivy magit projectile multiple-cursors counsel which-key vimish-fold use-package org-bullets moody minions key-chord helpful gruvbox-theme evil-surround evil-nerd-commenter evil-magit evil-leader dumb-jump counsel-projectile company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
