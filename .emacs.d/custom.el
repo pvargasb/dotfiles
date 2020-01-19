4fac025dd66f7a3cfc0ba37ebbb0f9201f7e473c
Pablo <pvarber@outlook.es>
2020-01-19 16:12:18 -0600

    Deleted evil-magit package

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (hungry-delete origami evil-escape zenburn-theme evil ivy magit projectile multiple-cursors counsel which-key vimish-fold use-package org-bullets moody minions helpful evil-surround evil-nerd-commenter evil-magit evil-leader dumb-jump counsel-projectile company))))
+    (hungry-delete origami evil-escape zenburn-theme evil ivy magit projectile multiple-cursors counsel which-key vimish-fold use-package org-bullets moody minions helpful evil-surround evil-nerd-commenter evil-leader dumb-jump counsel-projectile company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
