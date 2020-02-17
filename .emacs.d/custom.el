8215c233befe84c2029096432e4ef4eb3fa00c6f
Pablo <pvarber@outlook.es>
2020-02-17 16:05:46 -0600

    Reverted commits

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ispell-dictionary "es")
  '(package-selected-packages
    (quote
     (markdown-mode hungry-delete origami evil-escape zenburn-theme evil ivy magit projectile multiple-cursors counsel which-key vimish-fold use-package org-bullets moody minions helpful evil-surround evil-nerd-commenter evil-leader dumb-jump counsel-projectile company))))
