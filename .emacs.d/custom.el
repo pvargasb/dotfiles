dff80bd8279b73994cec2c26bcdef87bbdcc1b35
Pablo <pvarber@outlook.es>
2020-04-27 15:30:13 -0500

    Marked as installed some packages

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (color-theme-sanityinc-tomorrow markdown-mode hungry-delete origami evil-escape zenburn-theme evil ivy magit projectile multiple-cursors counsel which-key vimish-fold use-package org-bullets moody minions helpful evil-surround evil-nerd-commenter evil-leader dumb-jump counsel-projectile company))))
+    (flycheck solarized-theme yasnippet yasnippet-snippets color-theme-sanityinc-tomorrow markdown-mode hungry-delete origami evil-escape zenburn-theme evil ivy magit projectile multiple-cursors counsel which-key vimish-fold use-package org-bullets moody minions helpful evil-surround evil-nerd-commenter evil-leader dumb-jump counsel-projectile company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
