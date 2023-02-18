fbd55a581edf3ad75ba5cc95c7da99989fc74127
Pablo <49590991+PabloVarg@users.noreply.github.com>
2023-02-18 17:31:03 -0600

    feat(nvim): Install and configure formatter

---
+++ .config/nvim/after/plugin/formatter.lua
@@ -0,0 +1,23 @@
+require("formatter").setup {
+    logging = false,
+
+    filetype = {
+        lua = {
+            require("formatter.filetypes.lua").stylua
+        },
+        python = {
+            require("formatter.filetypes.python").black
+        },
+        ["*"] = {
+            require("formatter.filetypes.any").remove_trailing_whitespace
+        }
+    }
+}
+
+local augroup = vim.api.nvim_create_augroup("formatter", {})
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    group = augroup,
+    pattern = "*",
+    command = [[FormatWrite]]
+})
