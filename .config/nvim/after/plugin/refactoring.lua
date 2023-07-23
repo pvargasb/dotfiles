ea9baa9da17747dd8f97b968a9f57452b13ebc6e
Pablo <49590991+PabloVarg@users.noreply.github.com>
2023-07-22 18:32:29 -0600

    feat(nvim): Add refactorings debug keymaps

--- .config/nvim/after/plugin/refactoring.lua
+++ .config/nvim/after/plugin/refactoring.lua
@@ -4,3 +4,15 @@ vim.api.nvim_set_keymap(
 	[[<Esc><cmd>silent! lua require('telescope').extensions.refactoring.refactors()<CR>]],
 	{ noremap = true }
 )
+
+vim.keymap.set("n", "<leader>ep", function()
+	require("refactoring").debug.printf()
+end)
+
+vim.keymap.set("x", "<leader>ep", function()
+	require("refactoring").debug.print_var()
+end)
+
+vim.keymap.set("n", "<leader>ec", function()
+	require("refactoring").debug.cleanup({})
+end)
