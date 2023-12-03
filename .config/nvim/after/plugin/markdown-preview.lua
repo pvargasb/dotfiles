5bcb0339eb6ed2d981196af876be17eb74092a3d
Pablo <49590991+PabloVarg@users.noreply.github.com>
2023-12-02 19:47:30 -0600

    feat(nvim): Change browser for markdown preview

--- .config/nvim/after/plugin/markdown-preview.lua
+++ .config/nvim/after/plugin/markdown-preview.lua
@@ -2,7 +2,7 @@ vim.g.mkdp_auto_close = 0
 vim.g.mkdp_browserfunc = "g:Open_browser_incognito"
 vim.g.Open_browser_incognito = vim.cmd([[
 function! g:Open_browser_incognito(url)
-    silent exec "Start! $BROWSER --incognito --app=" . a:url
+    silent exec "Start! $BROWSER --private-window " . a:url
 endfunction
 ]])
 
