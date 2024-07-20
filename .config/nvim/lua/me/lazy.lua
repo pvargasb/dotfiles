b17a319c6b0c50b4985802bcebc8b41a5dde42cf
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-07-19 22:32:12 -0600

    feat(nvim): Change statusbar to lualine

--- .config/nvim/lua/me/lazy.lua
+++ .config/nvim/lua/me/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
 
-	"itchyny/lightline.vim",
+	"nvim-lualine/lualine.nvim",
 	"christoomey/vim-tmux-navigator",
 	"mbbill/undotree",
 	"tpope/vim-fugitive",
