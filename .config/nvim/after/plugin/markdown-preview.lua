618a2daf8e47f03c72ec62467e2e726d4e85a116
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-07-26 18:42:53 -0600

    feat(nvim): Prepare markdown to serve on local network

--- .config/nvim/after/plugin/markdown-preview.lua
+++ .config/nvim/after/plugin/markdown-preview.lua
@@ -1,4 +1,6 @@
 vim.g.mkdp_auto_close = 0
+vim.g.mkdp_echo_preview_url = 1
+vim.g.mkdp_open_to_the_world = 0
 vim.g.mkdp_browserfunc = "g:Open_browser_incognito"
 vim.g.Open_browser_incognito = vim.cmd([[
 function! g:Open_browser_incognito(url)
