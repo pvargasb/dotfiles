4c371f4a7c69943ce873c5152c9dcdcc532dfd36
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-09-16 14:45:25 -0600

    feat(nvim): Move betwenn quickfik elements

--- .config/nvim/lua/me/remaps.lua
+++ .config/nvim/lua/me/remaps.lua
@@ -57,3 +57,12 @@ vim.keymap.set("n", "tq", function()
 		vim.cmd("copen")
 	end
 end)
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  callback = function(event)
+    local opts = { buffer = event.buf, silent = true }
+    vim.keymap.set('n', '<C-n>', '<cmd>cn | wincmd p<CR>', opts)
+    vim.keymap.set('n', '<C-p>', '<cmd>cN | wincmd p<CR>', opts)
+  end,
+})
